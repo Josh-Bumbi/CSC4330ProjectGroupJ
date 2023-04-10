3ace5ae7f4e48af97e03163ef516df94939cdd91
Joshua Villere <joshvill234@gmail.com>
2023-04-10 15:53:59 -0500

    added js functionality to tutor page

--- profiles/tutor.js
+++ profiles/tutor.js
@@ -1,9 +1,9 @@
-import { getUser } from "../public/js/Services/databaseServices";
-import { User } from "../public/js/model/user";
+import { getUser } from "../public/js/Services/databaseServices.js";
+import { User } from "../public/js/model/user.js";
 
-getUser("qJ6ipz12ooMlIjkuDYqtnxTD6su2").then((User) => {
-    var tutorName = User.name;
-    var tutorMajor = User.major;
+getUser("qJ6ipz12ooMlIjkuDYqtnxTD6su2").then((user) => {
+    var tutorName = user.name;
+    var tutorMajor = user.major;
     document.getElementById('major').innerHTML = tutorMajor;
     document.getElementById('name').innerHTML = tutorName;
 })
