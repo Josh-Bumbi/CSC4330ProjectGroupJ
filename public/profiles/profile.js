56c67813648b7b88bc378a0a65b0f15a6fd93bf4
Joshua Villere <joshvill234@gmail.com>
2023-04-23 16:46:58 -0500

    small changes (temp)

--- public/profiles/profile.js
+++ public/profiles/profile.js
@@ -1,5 +1,19 @@
-import { getUser } from "../public/js/services/databaseServices.js";
-import { User } from "../public/js/model/user.js";
+import { getUser,getCurrentUser } from "../public/js/services/databaseServices.js";
+
+var student;
+
+// TESTING
+signInUser("student1@lsu.edu", "Password12345!").then((userId) => {
+	console.log(userId.uid)
+	getCurrentUser().then((user) => {
+		student = user;
+        console.log(user);
+        var userType = user.userType;
+        
+	})
+
+});
+//END TESTING
 
 getUser("qJ6ipz12ooMlIjkuDYqtnxTD6su2").then((user) => {
     var userName = user.name;
