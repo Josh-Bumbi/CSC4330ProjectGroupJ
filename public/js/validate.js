d36181a69a70f05b18b22272dd4aeec66ca62200
sstep25 <113745894+sstep25@users.noreply.github.com>
2023-04-25 17:48:45 -0700

    Add files via upload

--- public/js/validate.js
+++ public/js/validate.js
@@ -1,56 +1,59 @@
-import { signInUser, createUser } from "./services/authServices.js";
-import { User, UserType } from "./model/user.js";
-import { writeUserData, getCurrentUser} from "./services/databaseServices.js";
+import {signInUser, createUser} from "./services/authServices.js";
+import {User, UserType} from "./model/user.js";
+import {writeUserData, getCurrentUser} from "./services/databaseServices.js";
+
 const firstName = document.querySelector('#firstName');
 const uEmail = document.querySelector('#uEmail');
 const major = document.querySelector('#major');
 const grade = document.querySelector('#grade');
-
 const password = document.querySelector('#password');
 const signUpForm = document.querySelector('#signupForm');
-
-
 const signInForm = document.querySelector('#signinForm');
-
 
 const isRequired = value => value === '' ? false : true;
 const isBetween = (length, min, max) => length < min || length > max ? false : true;
 
-signUpForm.addEventListener('submit', function (e) {
+signUpForm.addEventListener('submit', function (e)
+{
   e.preventDefault();
-  //get the values of the inputs
 
+  //get the values of the inputs
   const firstNameValue = firstName.value.trim();
   const uEmailValue = uEmail.value.trim();
   const majorValue = major.value.trim();
   const gradeValue = grade.value.trim();
   const passwordValue = password.value.trim();
-  //run through test functions for each input
-  if(checkFirstName()  && checkEmail() && checkPassword() && major.value != "" && grade.value != "")
-  {
 
+  //run through test functions for each input
+  if(checkFirstName()  && checkEmail() && checkPassword() && checkMajor() && checkGrade())
+  {
     var newUser  = new User(null, firstNameValue, uEmailValue, UserType.STUDENT, gradeValue, majorValue);
 
-    createUser(uEmailValue, passwordValue).then((user) => {
-        if (user != null) {
-            //User authentication was successful, push user data to database
-          newUser.userId = user.uid;
-          writeUserData(newUser).then( _ => {
-              window.alert("User successfully created!");
-              window.location.href = 'StudentHome.html';
-          })
-        }
-    })
-  } else {
+    createUser(uEmailValue, passwordValue).then((user) =>
+    {
+      if (user != null)
+      {
+        //User authentication was successful, push user data to database
+        newUser.userId = user.uid;
 
+        writeUserData(newUser).then( _ =>
+        {
+          window.alert("User successfully created!");
+          window.location.href = 'StudentHome.html';
+        })
+      }
+    })
+  }
+  else
+  {
     alert("Please make sure the form is completely filled out.");
   }
-
 });
 
 // const signInEmail = document.getElementById('signInEmail');
 // console.log(signInEmail)
-signInForm.addEventListener('submit', function (e) {
+signInForm.addEventListener('submit', function (e)
+{
   e.preventDefault();
 
   const signInEmail = document.getElementById('signInEmail');
@@ -58,28 +61,34 @@ signInForm.addEventListener('submit', function (e) {
 
   if(signInEmail && signInPassword && signInEmail.value != "" && signInPassword.value != "")
   {
-    signInUser(signInEmail.value, signInPassword.value).then(async (userId) => {
-      if (userId != null) {
+    signInUser(signInEmail.value, signInPassword.value).then(async(userId) =>
+    {
+      if (userId != null)
+      {
         var user = await getCurrentUser();
-        if (user.userType == "student") {
+
+        if (user.userType == "student")
+        {
           window.location.href = 'StudentHome.html';
-        } else if (user.userType == "tutor") {
+        }
+        else if (user.userType == "tutor")
+        {
           window.location.href = 'TutorHome.html';
-        } else if (user.userType == "admin") {
+        }
+        else if (user.userType == "admin")
+        {
           window.location.href = 'AdminHome.html';
         }
-        
       }
     });
-  } else {
+  }
+  else
+  {
     alert("Please enter a valid email and password");
   }
-
 });
 
-
-
-const debounce = (fn, delay = 500) =>
+const debounce = (fn, delay = 250) =>
 {
   let timeoutId;
 
@@ -109,6 +118,12 @@ signUpForm.addEventListener('input', debounce(function (e)
     case 'password':
       checkPassword();
       break;
+    case 'major':
+      checkMajor();
+      break;
+    case 'grade':
+      checkGrade();
+      break;
   }
 }));
 
@@ -124,6 +139,20 @@ const isPasswordSecure = (password) =>
   const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})");
   return re.test(password);
 };
+
+const isMajorValid = (major) =>
+{
+  //Name of major is required to have at least 3 characters
+  const re = new RegExp("^(?=.{3,})");
+  return re.test(major);
+}
+
+const isGradeValid = (grade) =>
+{
+  //grade is required to have one and only one number
+  const re = new RegExp('^[0-9]{1,}$');
+  return re.test(grade);
+}
 
 //Shows an error message
 const showError = (input, message) =>
@@ -167,7 +196,7 @@ const checkFirstName = () =>
   }
   else if(!isBetween(fName.length, min, max))
   {
-    showError(firstName, `Field must be between ${min}-${max} characters.`)
+    showError(firstName, `Invalid Name`);
   }
   else
   {
@@ -179,27 +208,73 @@ const checkFirstName = () =>
 }
 
 //Checks if the last name submitted is between 3 and 50 characters long
-const checkLastName = () =>
+// const checkLastName = () =>
+// {
+//   let valid = false;
+//   const min = 3;
+//   const max = 50;
+//   const lName = lastName.value.trim();
+
+//   if(!isRequired(lName))
+//   {
+//     showError(lastName, 'This field cannot be blank.');
+//   }
+//   else if(!isBetween(lName.length, min, max))
+//   {
+//     showError(lastName, `Field must be between ${min}-${max} characters.`)
+//   }
+//   else
+//   {
+//     showSuccess(lastName);
+//     valid = true;
+//   }
+  
+//   return valid;
+// }
+
+//Checks if grade submitted is valid
+const checkGrade = () =>
 {
   let valid = false;
-  const min = 3;
-  const max = 50;
-  const lName = lastName.value.trim();
+  const inputGrade = grade.value.trim();
 
-  if(!isRequired(lName))
+  if(!isRequired(inputGrade))
   {
-    showError(lastName, 'This field cannot be blank.');
+    showError(grade, 'This field cannot be blank.');
   }
-  else if(!isBetween(lName.length, min, max))
+  else if(!isGradeValid(inputGrade))
   {
-    showError(lastName, `Field must be between ${min}-${max} characters.`)
+    showError(grade, 'Invalid Input');
   }
   else
   {
-    showSuccess(lastName);
+    showSuccess(grade);
     valid = true;
   }
-  
+
+  return valid;
+}
+
+//Checks if major submitted is valid
+const checkMajor = () =>
+{
+  let valid = false;
+  const inputMajor = major.value.trim();
+
+  if(!isRequired(inputMajor))
+  {
+    showError(major, 'This field cannot be blank.');
+  }
+  else if(!isMajorValid(inputMajor))
+  {
+    showError(major, 'Invalid Input');
+  }
+  else
+  {
+    showSuccess(major);
+    valid = true;
+  }
+
   return valid;
 }
 
@@ -215,7 +290,7 @@ const checkEmail = () =>
   }
   else if(!isEmailValid(email) || email.slice(-4) != '.edu')
   {
-    showError(uEmail, 'University Email is not valid.')
+    showError(uEmail, 'Invalid Email');
   }
   else
   {
@@ -226,11 +301,12 @@ const checkEmail = () =>
   return valid;
 }
 
+//
+
 //Checks if the password submitted is valid
 const checkPassword = () =>
 {
   let valid = false;
-
   const tPassword = password.value.trim();
 
   if(!isRequired(tPassword))
