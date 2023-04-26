df15cdd369ca88efeb88c5e40cf5cf8879082e36
Joshua Villere <joshvill234@gmail.com>
2023-04-25 19:08:27 -0500

    added displayed rating for tutors on profiles

--- public/profile.js
+++ public/profile.js
@@ -1,4 +1,4 @@
-import { getUser,getCurrentUser } from "./js/services/databaseServices.js";
+import { getUser,getCurrentUser,getAllReviews } from "./js/services/databaseServices.js";
 import { signInUser, logout } from "./js/services/authServices.js";
 
 function getIdFromURL() {
@@ -20,6 +20,35 @@ var viewedUser = await getUser(getIdFromURL('id'));
 
 var userName = viewedUser.name;
 var userMajor = viewedUser.major;
+
+//Finds the average rating to the nearest whole number
+var ratings = await getAllReviews(viewedUser.userId);
+var averageRating = 0;
+var i = 0;
+while(i < ratings.length){
+    averageRating = averageRating + parseInt(ratings[i].stars);
+    i++;
+}
+averageRating = averageRating/i;
+averageRating = Math.round(averageRating);
+console.log(averageRating);
+
+if(averageRating == 1){
+    document.getElementById('rating').innerHTML = "1/5 ★☆☆☆☆";
+}
+else if(averageRating == 2){
+    document.getElementById('rating').innerHTML = "2/5 ★★☆☆☆";
+}
+else if(averageRating == 3){
+    document.getElementById('rating').innerHTML = "3/5 ★★★☆☆";
+}
+else if(averageRating == 4){
+    document.getElementById('rating').innerHTML = "4/5 ★★★★☆";
+}
+else if(averageRating == 5){
+    document.getElementById('rating').innerHTML = "5/5 ★★★★★";
+}
+
 document.getElementById('major').innerHTML = userMajor;
 document.getElementById('name').innerHTML = userName;
 
@@ -33,8 +62,6 @@ if (currentUserType == "student") {
     });
 
 }
-console.log(currentUserType); 
-
 
 // // TESTING
 // signInUser("student1@lsu.edu", "Password12345!").then((userId) => {
